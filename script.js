b8bb8976e5b0324160610bbb1b0f1f3eaf008e8d
cbtrix <clarab.snts12@gmail.com>
2026-03-15 20:02:32 -0300

    End Commit!

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ function limpar()
 {
     resultado.textContent = '0'
 }
+let numeroAnterior =''
+let operadorAtual = ''
+
+function clicarOperador(op) {
+    numeroAnterior = resultado.textContent
+    operadorAtual = op
+    resultado.textContent = '0'
+}
+function calcular() {
+  const num1 = parseFloat(numeroAnterior)
+  const num2 = parseFloat(resultado.textContent)
+  let conta = 0
+
+  if (operadorAtual === '+') conta = num1 + num2
+  if (operadorAtual === '-') conta = num1 - num2
+  if (operadorAtual === '×') conta = num1 * num2
+  if (operadorAtual === '÷') conta = num1 / num2
+
+  resultado.textContent = conta
+}
+function calcularPorcentagem() {
+  resultado.textContent = parseFloat(resultado.textContent) / 100
+}
+function inverterSinal() {
+  resultado.textContent = parseFloat(resultado.textContent) * -1
+}
