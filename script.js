d8d00fde4117a626ec008e509aac6e0bb3db20b0
cbtrix <clarab.snts12@gmail.com>
2026-03-15 19:45:48 -0300

    clean

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const resultado = document.getElementById('resultado')
 
 function clicarNumero(numero) {
-  resultado.textContent = numero
+ if (resultado.textContent === '0'){
+    resultado.textContent = numero
+ }
+ else {
+    resultado.textContent += numero
+ }
+}
+function limpar()
+{
+    resultado.textContent = '0'
 }
